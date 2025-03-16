06902e15dea92a4e37cfbafec7894208e6ee97f2
blkeerthi <blkeerthi1999@gmail.com>
2025-03-16 20:50:26 +0530

    amazon cloud storage changes

--- src/main/java/com/example/Amazon/s3/image/strorage/AmazonStorage/AmazonS3Service/AmzS3StorageService.java
+++ src/main/java/com/example/Amazon/s3/image/strorage/AmazonStorage/AmazonS3Service/AmzS3StorageService.java
@@ -2,11 +2,6 @@ package com.example.Amazon.s3.image.strorage.AmazonStorage.AmazonS3Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.example.Amazon.s3.image.strorage.AmazonStorage.entity.FileInfo;
-import com.example.Amazon.s3.image.strorage.AmazonStorage.entity.SPBranding;
-import com.example.Amazon.s3.image.strorage.AmazonStorage.entity.SpBrandingInfo;
-import com.example.Amazon.s3.image.strorage.AmazonStorage.repositorys.SpBrandingInfoRepository;
-import com.example.Amazon.s3.image.strorage.AmazonStorage.repositorys.SpBrandingRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +13,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 @Service
 public class AmzS3StorageService {
@@ -30,43 +24,15 @@ public class AmzS3StorageService {
     private String bucketName;
 
     @Autowired
-    private SpBrandingRepository spBrandingRepository;
-
-    @Autowired
-    private SpBrandingInfoRepository spBrandingInfoRepository;
-
-    @Autowired
     private AmazonS3 s3;
 
-    public void uploadImage(List<MultipartFile> files, SPBranding spBranding) throws FileNotFoundException {
-        List<SpBrandingInfo> brandingUrlsList = new ArrayList<>();
-        Map<String, MultipartFile> fileMap = files.stream()
-                .collect(Collectors.toMap(MultipartFile::getOriginalFilename, file -> file));
-
-        Map<String,MultipartFile> matchedFiles = getFilesForBranding(fileMap, spBranding);
-
-        if (matchedFiles.isEmpty()) {
-            throw new RuntimeException("No valid branding files found. Upload at least one.");
-        }
-        for (Map.Entry<String, MultipartFile> entry : matchedFiles.entrySet()) {
-            MultipartFile file = entry.getValue();
-            File fileonj = convertMultipartFile(file);
-            String filename= file.getOriginalFilename();
-            s3.putObject(new PutObjectRequest(bucketName, filename, fileonj));
-            String imageUrl = s3.getUrl(bucketName,filename).toString();
-            SpBrandingInfo spBrandingInfo = new SpBrandingInfo();
-            spBrandingInfo.setName(entry.getKey());
-            spBrandingInfo.setValue(imageUrl);
-            spBrandingInfo.setSpBranding(spBranding);  // Associate with SPBranding
-            brandingUrlsList.add(spBrandingInfo);
-            fileonj.delete();
-        }
-     spBranding.getBrandingInfoList().addAll(brandingUrlsList);
-        spBrandingRepository.save(spBranding);
+    public String uploadImage(MultipartFile multipartFile) throws FileNotFoundException {
+        File fileonj = convertMultipartFile(multipartFile);
+        String filename= System.currentTimeMillis()+"_"+multipartFile.getOriginalFilename();
+        s3.putObject(new PutObjectRequest(bucketName,filename,fileonj));
+        fileonj.delete();
+        return "File Uploaded " + filename;
     }
-
-
-
 
     private File convertMultipartFile(MultipartFile file) throws FileNotFoundException {
         File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
@@ -77,61 +43,5 @@ public class AmzS3StorageService {
             throw new RuntimeException(e);
         }
         return convertedFile;
-    }
-    private Map<String,MultipartFile> getFilesForBranding(Map<String, MultipartFile> fileMap, SPBranding spBranding) {
-
-        Map<String,MultipartFile> matchedFiles = new HashMap<>();
-        if (spBranding.getFilesInfo()!= null) {
-            for (FileInfo fileInfo : spBranding.getFilesInfo()) {
-                String fileName = fileInfo.getFileName();
-
-                if (fileName != null) {
-                    switch (fileInfo.getName()) {
-                        case "ImageLogo":
-                            matchedFiles.put("ImageLogo",fileMap.get(fileName));
-                            break;
-                        case "BackgroundImage":
-                            matchedFiles.put("BackgroundImage",fileMap.get(fileName));
-                            break;
-                        case "UserIcon":
-                            matchedFiles.put("UserIcon",fileMap.get(fileName));
-                            break;
-                        default:
-                            System.out.println("Unknown file type: " + fileInfo.getName());
-                            break;
-                    }
-                } else {
-                    System.out.println("No matching file found for: " + fileInfo.getName());
-                }
-            }
-        }
-        return matchedFiles;
-    }
-    public Map<String, Object> getBrandingConfiguration(String brandingName) {
-        SPBranding spBranding = spBrandingRepository.findByName(brandingName);
-        if (spBranding == null) {
-            return Collections.emptyMap();  // Return empty response if branding not found
-        }
-
-        List<SpBrandingInfo> brandingInfos = spBrandingInfoRepository.findBySpBrandingId(spBranding.getId());
-
-        // Prepare response
-        Map<String, Object> response = new HashMap<>();
-        response.put("spDisplayName", spBranding.getSpDisplayName());
-
-        List<Map<String, String>> filesInfo = brandingInfos.stream()
-                .map(info -> Map.of(
-                        "name", info.getName(),
-                        "fileName", info.getValue()))
-                .collect(Collectors.toList());
-
-        response.put("filesInfo", filesInfo);
-        return response;
-    }
-
-    public SPBranding getDefaultBrandingConfiguration() {
-        SPBranding defaultBranding = new SPBranding();
-        defaultBranding.setSpDisplayName("Default Branding");
-        return defaultBranding;
     }
 }
