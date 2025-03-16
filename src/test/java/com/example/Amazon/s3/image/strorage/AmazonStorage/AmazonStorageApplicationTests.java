65b97966af765a9de68a6b37141f753f718490db
blkeerthi <blkeerthi1999@gmail.com>
2025-03-16 10:36:08 +0530

    my first commit

---
+++ src/test/java/com/example/Amazon/s3/image/strorage/AmazonStorage/AmazonStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Amazon.s3.image.strorage.AmazonStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
